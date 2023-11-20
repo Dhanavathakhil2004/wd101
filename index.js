f9b01599fdfb823df4b41556d729aa152ff3f477
Dhanavathakhil2004 <151482809+Dhanavathakhil2004@users.noreply.github.com>
2023-11-20 20:55:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
-document.getElementById("registrationForm").addEventListener("submit", function(event) {
+// script.js file
+
+// Function to save form data to localStorage
+function saveFormData(name, email, password, dob, acceptedTerms) {
+  const entry = { name, email, password, dob, acceptedTerms };
+  let entries = JSON.parse(localStorage.getItem('entries')) || [];
+  entries.push(entry);
+  localStorage.setItem('entries', JSON.stringify(entries));
+}
+
+// Function to load form data from localStorage
+function loadFormData() {
+  const entries = JSON.parse(localStorage.getItem('entries')) || [];
+  const tableBody = document.querySelector('#entries tbody');
+
+  tableBody.innerHTML = ''; // Clear existing entries before re-rendering
+
+  entries.forEach(entry => {
+    const newRow = tableBody.insertRow();
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
+    const cell3 = newRow.insertCell(2);
+    const cell4 = newRow.insertCell(3);
+    const cell5 = newRow.insertCell(4);
+
+    cell1.textContent = entry.name;
+    cell2.textContent = entry.email;
+    cell3.textContent = entry.password;
+    cell4.textContent = entry.dob;
+    cell5.textContent = entry.acceptedTerms ? 'Yes' : 'No';
+  });
+}
+
+// Load existing form data when the page loads
+document.addEventListener('DOMContentLoaded', loadFormData);
+
+document.getElementById('registrationForm').addEventListener('submit', function(event) {
   event.preventDefault();
 
   // Gather form data
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-  const dob = document.getElementById("dob").value;
-  const acceptedTerms = document.getElementById("acceptedTerms").checked;
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const dob = document.getElementById('dob').value;
+  const acceptedTerms = document.getElementById('acceptedTerms').checked;
 
   // Validate age
   const today = new Date();
@@ -20,26 +56,18 @@ document.getElementById("registrationForm").addEventListener("submit", function(
   // Validate age and email
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (age < 18 || age > 55) {
-    alert("Age should be between 18 and 55 years old.");
+    alert('Age should be between 18 and 55 years old.');
   } else if (!emailRegex.test(email)) {
-    alert("Invalid email address.");
+    alert('Invalid email address.');
   } else {
-    // Create a new table row with the form data
-    const table = document.getElementById("entries").getElementsByTagName("tbody")[0];
-    const newRow = table.insertRow();
-    const cell1 = newRow.insertCell(0);
-    const cell2 = newRow.insertCell(1);
-    const cell3 = newRow.insertCell(2);
-    const cell4 = newRow.insertCell(3);
-    const cell5 = newRow.insertCell(4);
+    // Save form data to localStorage
+    saveFormData(name, email, password, dob, acceptedTerms);
 
-    cell1.textContent = name;
-    cell2.textContent = email;
-    cell3.textContent = password;
-    cell4.textContent = dob;
-    cell5.textContent = acceptedTerms ? "Yes" : "No";
+    // Reload table with updated form data
+    loadFormData();
 
     // Reset the form after submission
-    document.getElementById("registrationForm").reset();
+    document.getElementById('registrationForm').reset();
   }
 });
+
