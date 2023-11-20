650c3eda7794f15c3e4b208ef296203c30918038
Dhanavathakhil2004 <151482809+Dhanavathakhil2004@users.noreply.github.com>
2023-11-20 20:40:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ document.getElementById("registrationForm").addEventListener("submit", function(
   const dob = document.getElementById("dob").value;
   const acceptedTerms = document.getElementById("acceptedTerms").checked;
 
-  // Validate age (assuming today's date is used)
+  // Validate age
   const today = new Date();
   const birthDate = new Date(dob);
   const age = today.getFullYear() - birthDate.getFullYear();
@@ -17,33 +17,29 @@ document.getElementById("registrationForm").addEventListener("submit", function(
     age--;
   }
 
+  // Validate age and email
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (age < 18 || age > 55) {
     alert("Age should be between 18 and 55 years old.");
-    return;
-  }
-
-  // Validate email
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailRegex.test(email)) {
+  } else if (!emailRegex.test(email)) {
     alert("Invalid email address.");
-    return;
+  } else {
+    // Create a new table row with the form data
+    const table = document.getElementById("entries").getElementsByTagName("tbody")[0];
+    const newRow = table.insertRow();
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
+    const cell3 = newRow.insertCell(2);
+    const cell4 = newRow.insertCell(3);
+    const cell5 = newRow.insertCell(4);
+
+    cell1.textContent = name;
+    cell2.textContent = email;
+    cell3.textContent = password;
+    cell4.textContent = dob;
+    cell5.textContent = acceptedTerms ? "Yes" : "No";
+
+    // Reset the form after submission
+    document.getElementById("registrationForm").reset();
   }
-
-  // Create a new table row with the form data
-  const table = document.getElementById("entries").getElementsByTagName("tbody")[0];
-  const newRow = table.insertRow();
-  const cell1 = newRow.insertCell(0);
-  const cell2 = newRow.insertCell(1);
-  const cell3 = newRow.insertCell(2);
-  const cell4 = newRow.insertCell(3);
-  const cell5 = newRow.insertCell(4);
-
-  cell1.innerHTML = name;
-  cell2.innerHTML = email;
-  cell3.innerHTML = password;
-  cell4.innerHTML = dob;
-  cell5.innerHTML = acceptedTerms ? "Yes" : "No";
-
-  // Reset the form after submission
-  document.getElementById("registrationForm").reset();
 });
