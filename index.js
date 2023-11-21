196a0b547839119f3ad1c1fe4da84057390eefa4
Dhanavathakhil2004 <151482809+Dhanavathakhil2004@users.noreply.github.com>
2023-11-21 19:47:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 document.getElementById('registrationForm').addEventListener('submit', function(event) {
   event.preventDefault();
 
-  // Gather form data
   const name = document.getElementById('name').value;
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
   const dob = document.getElementById('dob').value;
   const terms = document.getElementById('terms').checked ? 'Yes' : 'No';
 
-  // Validate email and age
   const isValidEmail = validateEmail(email);
   const age = calculateAge(dob);
 
@@ -22,22 +20,7 @@ document.getElementById('registrationForm').addEventListener('submit', function(
     return;
   }
 
-  // Add entry to the table
-  const table = document.getElementById('userTable').getElementsByTagName('tbody')[0];
-  const newRow = table.insertRow(-1);
-  const cell1 = newRow.insertCell(0);
-  const cell2 = newRow.insertCell(1);
-  const cell3 = newRow.insertCell(2);
-  const cell4 = newRow.insertCell(3);
-  const cell5 = newRow.insertCell(4);
-
-  cell1.innerHTML = name;
-  cell2.innerHTML = email;
-  cell3.innerHTML = password;
-  cell4.innerHTML = dob;
-  cell5.innerHTML = terms;
-
-  // Clear form fields after submission
+  addToTable(name, email, password, dob, terms);
   document.getElementById('registrationForm').reset();
 });
 
@@ -58,6 +41,24 @@ function calculateAge(dob) {
 
   return age;
 }
+
+function addToTable(name, email, password, dob, terms) {
+  const tableBody = document.getElementById('userTableBody');
+  const newRow = tableBody.insertRow();
+
+  const cell1 = newRow.insertCell(0);
+  const cell2 = newRow.insertCell(1);
+  const cell3 = newRow.insertCell(2);
+  const cell4 = newRow.insertCell(3);
+  const cell5 = newRow.insertCell(4);
+
+  cell1.textContent = name;
+  cell2.textContent = email;
+  cell3.textContent = password;
+  cell4.textContent = dob;
+  cell5.textContent = terms;
+}
+
 
 
 
